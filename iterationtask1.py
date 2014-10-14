ec41610f1e995e836c36d6fedbc369fff46fa79f
Kieran Burnett <40586@longroad.ac.uk>
2014-10-14 21:24:18 +0100

    old

---
+++ iterationtask1.py
@@ -0,0 +1,9 @@
+#kieran burnett
+#13-10-2014
+#itteration task 1
+
+message = input('Please enter a message: ')
+times = int(input('Please enter the ammount of times you want it printed'))
+
+for count in range(times):
+    print(message)
---
+++ iterationtask2.py
@@ -0,0 +1,10 @@
+#kieran burnett
+#13-10-2014
+#itteration task 2
+
+password = (input('Please enter a the password: '))
+
+while password != 'secret':
+    password = (input('Incorrect. Please enter a the password again: '))
+
+print('You now have access to the system')
